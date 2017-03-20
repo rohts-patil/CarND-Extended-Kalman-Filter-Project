d8df7fad673271d47955d0a80a1c3f5d5c079fd2
scbrubaker02 <scbrubaker02@gmail.com>
2017-03-20 14:13:51 -0400

    Draft of .udacity-pa folder

---
+++ .udacity-pa/projects.py
@@ -0,0 +1,33 @@
+import argparse
+from udacity_pa import udacity
+
+nanodegree = 'nd013'
+projects = ['extended-kalman-filter']
+
+def submit(args):
+  parser = argparse.ArgumentParser(description='submits the project.')
+  parser.add_argument('filenames', nargs='*', help='files to include (those included as starter code by default')
+
+  local_args = parser.parse_args(args.args)
+
+  default_filenames = [
+    'src/FusionEKF.cpp',
+    'src/FusionEKF.h',
+    'src/kalman_filter.cpp',
+    'src/kalman_filter.h',
+    'src/measurement_package.h',
+    'src/rmse.cpp',
+    'src/tools.cpp',
+    'src/tools.h'
+  ]
+
+  if len(args.files) == 0:
+    filenames = default_filenames
+  else:
+    filenames = args.filenames
+
+  print(filenames)
+
+  udacity.submit(nanodegree, projects[0], filenames, 
+                 environment = args.environment,
+                 jwt_path = args.jwt_path)
