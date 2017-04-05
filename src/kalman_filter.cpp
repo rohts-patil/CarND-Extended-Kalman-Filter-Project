6451fdcb882d562562115932e4944f814112e993
Rohit Patil <rohts.patil@gmail.com>
2017-04-06 01:11:01 +0530

    Added changes for predict and update Radar and Lidar data

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -22,6 +22,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_; // More general: x_ = F_ * x_ + u_ assume u=0 here
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +32,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +52,33 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  double rho = sqrt((x_[0] * x_[0]) + (x_[1] * x_[1]));
+  double phi;
+  double rho_dot;
+
+  if (x_[0] != 0) {
+    phi= atan2(x_[1], x_[0]);
+    rho_dot = ((x_[0] * x_[2] + x_[1] * x_[3]) / rho);
+  } else {
+    phi= 0;
+    rho_dot = 0;
+  }
+
+  MatrixXd z_pred(3, 1);
+  z_pred << rho, phi, rho_dot;
+
+  //VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();       // pass Hj_ to H_
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;              //K: Kalman gain
+
+  // Update the estimate and new covariance
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I- K * H_) * P_;
 }
